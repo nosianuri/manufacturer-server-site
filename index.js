ee93015d58622492964879551f04ac4c36b943bc
nosianuri <nurinosia@gmail.com>
2022-05-26 09:45:22 +0600

    get review set

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const app = express();
 const port = process.env.PORT || 5000;
+const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpq6jie.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('manufacturer_site').collection('parts');
+        const orderCollection = client.db('manufacturer_site').collection('orders');
+        const reviewCollection = client.db('manufacturer_site').collection('reviews');
 
         app.put('user/:email', async (req, res) => {
             const email = req.params.email;
@@ -35,6 +37,30 @@ async function run(){
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/review', async(req, res) =>{
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.post('/reviews', async (req, res) => {
+            const newReview = req.body;
+            const result = await reviewCollection.insertOne(newReview);
+            res.send(result);
+        });
+
+        // use post to get services by ids
+        app.post('/productByKeys', async(req, res) =>{
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = {_id: {$in: ids}}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            console.log(keys);
             res.send(services);
         })
     }
