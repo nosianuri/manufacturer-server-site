3886d91231d263d4fb6e93322372e4c4bf3e6df4
nosianuri <nurinosia@gmail.com>
2022-05-26 23:54:30 +0600

    admin level access

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -13,33 +14,81 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpq6jie.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('manufacturer_site').collection('parts');
         const orderCollection = client.db('manufacturer_site').collection('orders');
         const reviewCollection = client.db('manufacturer_site').collection('reviews');
         const userCollection = client.db('manufacturer_site').collection('users');
 
-        app.put('user/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = { upset: true };
-            const updateDoc = {
-                $set: user,
-            };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send({result, token});
-        });
-
-        app.get('/service', async(req, res) =>{
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.get('/admin', verifyJWT, async(req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+          })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({email: requester});
+            if(requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: {role:'admin'},
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);  
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'forbidden'});
+            }
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        });
+
+        
+
+        
 
         // app.get('/available', async(req, res) =>{
         //     const available = req.query.available;
@@ -55,20 +104,26 @@ async function run(){
         //     res.send(services);
         // })
 
-        app.get('/order', async(req, res) =>{
+        app.get('/order', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = {email: email};
-            const orders = await orderCollection.find(query).toArray();
-            res.send(orders);
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const orders = await orderCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else{
+                return res.status(403).send({message: 'forbidden access'});
+            }
         })
 
-        app.post('/order', async(req, res) =>{
+        app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
-        app.get('/review', async(req, res) =>{
+        app.get('/review', async (req, res) => {
             const query = {};
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
@@ -82,18 +137,18 @@ async function run(){
         });
 
         // use post to get services by ids
-        app.post('/productByKeys', async(req, res) =>{
+        app.post('/productByKeys', async (req, res) => {
             const keys = req.body;
             const ids = keys.map(id => ObjectId(id));
-            const query = {_id: {$in: ids}}
+            const query = { _id: { $in: ids } }
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             console.log(keys);
             res.send(services);
         });
-        
+
     }
-    finally{
+    finally {
 
     }
 
@@ -102,9 +157,9 @@ async function run(){
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('Hello From Subaru MF Manufacturer Site !');
+    res.send('Hello From Subaru MF Manufacturer Site !');
 })
 
 app.listen(port, () => {
-  console.log(`Manufacturer app listening on port ${port}`)
+    console.log(`Manufacturer app listening on port ${port}`)
 })
